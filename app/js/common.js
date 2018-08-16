ace681a3ba382d85fb710de7d3f3f01d6cbd2d92
Павел Хрипунов <khripunovpp@gmail.com>
2018-08-16 10:22:20 +0300

    desktop

--- app/js/common.js
+++ app/js/common.js
@@ -1,21 +1,34 @@
 var menu = function() {
     var menu = $('.b-menu'),
+        link = $('.b-menu__link'),
         tail = $('.w-body'),
         toggleBtn = $('.b-header__toggle'),
         closeBtn = $('.b-menu__close');
 
     $(toggleBtn).on('click', function(event) {
         event.preventDefault();
-        $('body').addClass('menu-opened');
-        $(tail).addClass('translated');
-        $(menu).addClass('opened');
+        open()
     });
 
     $(closeBtn).on('click', function(event) {
         event.preventDefault();
+        close()
+    });
+
+    function open() {
+        $('body').addClass('menu-opened');
+        $(tail).addClass('translated');
+        $(menu).addClass('opened');
+    }
+
+    function close() {
         $(menu).removeClass('opened');
         $(tail).removeClass('translated');
         $('body').removeClass('menu-opened');
+    }
+
+    $(link).on('click', function() {
+        close()
     });
 }
 
@@ -25,7 +38,7 @@ var isFocus = function() {
     });
     $('.b-form__field').on('blur', function() {
         var value = $(this).val();
-        if(value.length == 0) $(this).closest('.b-form__group').find('.b-form__label').fadeIn(200);
+        if (value.length == 0) $(this).closest('.b-form__group').find('.b-form__label').fadeIn(200);
     });
 }
 
@@ -34,5 +47,13 @@ $(function() {
     menu()
 
     isFocus()
+
+    $('.b-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+    });
 
 });
